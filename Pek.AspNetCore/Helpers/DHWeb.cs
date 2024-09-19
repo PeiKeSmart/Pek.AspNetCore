53c4b871bf3058ce0dd73f6d65f9b0bed4d2cce8
猿人易 <qcjxberin@outlook.com>
2024-09-19 10:06:03 +0800

    更新包版本并新增未认证身份和HttpContext功能
    更新了 `Pek.AspNetCore.csproj` 文件中的 `Pek.Common` 包版本，从 `3.7.2024.913-beta1146` 更新到 `3.7.2024.919-beta0132`。
    
    在 `DHWeb.cs` 文件中新增了一个静态类 `DHWeb`，包含以下功能：
    - 获取当前的 `HttpContext`。
    - 获取当前用户的 `ClaimsPrincipal`。
    - 获取当前用户的 `ClaimsIdentity`。
    
    新增了 `UnauthenticatedIdentity.cs` 文件，定义了一个未认证的身份标识类 `UnauthenticatedIdentity`，继承自 `ClaimsIdentity`，并重写了 `IsAuthenticated` 属性，始终返回 `false`。
    
    新增了 `UnauthenticatedPrincipal.cs` 文件，定义了一个未认证的安全主体类 `UnauthenticatedPrincipal`，继承自 `ClaimsPrincipal`，并重写了 `Identity` 属性，返回 `UnauthenticatedIdentity` 实例。
    
    新增了 `HttpContext.cs` 文件，定义了一个静态类 `HttpContext`，用于获取和配置当前的 `HttpContext`，以及一个扩展方法 `UseStaticHttpContext`，用于配置静态 `HttpContext` 访问器。

---
+++ Pek.AspNetCore/Helpers/DHWeb.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Claims;
+
+using Pek.Security.Principals;
+
+namespace Pek.Helpers;
+
+/// <summary>
+/// Web操作
+/// </summary>
+public static partial class DHWeb
+{
+    #region HttpContext(当前Http上下文)
+
+    /// <summary>
+    /// 当前Http上下文
+    /// </summary>
+    public static HttpContext HttpContext => Pek.Webs.HttpContext.Current;
+
+    #endregion
+
+    #region User(当前用户安全主体)
+
+    /// <summary>
+    /// 当前用户安全主体
+    /// </summary>
+    public static ClaimsPrincipal User
+    {
+        get
+        {
+            if (HttpContext == null)
+                return UnauthenticatedPrincipal.Instance;
+            if (HttpContext.User is ClaimsPrincipal principal)
+                return principal;
+            return UnauthenticatedPrincipal.Instance;
+        }
+    }
+
+    #endregion
+
+    #region Identity(当前用户身份)
+
+    /// <summary>
+    /// 当前用户身份
+    /// </summary>
+    public static ClaimsIdentity Identity
+    {
+        get
+        {
+            if (User.Identity is ClaimsIdentity identity)
+                return identity;
+            return UnauthenticatedIdentity.Instance;
+        }
+    }
+
+    #endregion
+}
