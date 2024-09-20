3bbcfeecb644b54d9e38e7b63e23c8c9c71a8bfd
猿人易 <qcjxberin@outlook.com>
2024-09-20 16:31:33 +0800

    添加 PromptModel 和 ResultMsg 类及 HTTP 状态常量
    在 `PromptModel.cs` 文件中添加了 `PromptModel` 类，包含多个属性。
    在 `ResultMsg.cs` 文件中添加了 `ResultMsg` 类及其泛型版本，包含多个属性。
    在 `ResultMsg.cs` 文件中添加了 `HttpState` 静态类，定义了多个 HTTP 状态常量。

---
+++ Pek.AspNetCore/Models/Xmselect.cs
@@ -0,0 +1,17 @@
+﻿namespace Pek.Models;
+
+/// <summary>
+/// 下拉选项
+/// </summary>
+public class Xmselect<TModel>
+{
+    public string name { get; set; }
+
+    public TModel value { get; set; }
+
+    public bool disabled { get; set; } = false;
+
+    public Boolean selected { get; set; }
+
+    public String mutex { get; set; }
+}
