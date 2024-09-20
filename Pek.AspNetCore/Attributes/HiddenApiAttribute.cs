d343f4aecb346d1c24d2351031c8be6fc1ac7060
猿人易 <qcjxberin@outlook.com>
2024-09-20 16:48:54 +0800

    删除 HiddenApiAttribute.cs 文件中的所有代码
    删除了 `HiddenApiAttribute.cs` 文件中的所有代码，包括命名空间声明、注释、特性使用声明以及 `HiddenApiAttribute` 类的定义。

--- Pek.AspNetCore/Attributes/HiddenApiAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pek.Attributes;
-
-/// <summary>
-/// 隐藏swagger接口特性标识
-/// </summary>
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
-public class HiddenApiAttribute : System.Attribute
-{
-}
