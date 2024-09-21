80256083dc48b7cec4eab80bcc6b5eda8f43ad61
猿人易 <qcjxberin@outlook.com>
2024-09-21 09:28:25 +0800

    添加网络相关引用和属性方法
    添加对 `System.Net.NetworkInformation`、`System.Net.Sockets` 和 `System.Net` 的引用。
    在 `DHWeb` 类中添加静态构造函数初始化宿主环境。
    添加 `Environment` 属性获取宿主环境。
    添加 `Request` 属性获取当前 HTTP 请求。
    添加 `RefererUrl` 属性获取引用地址。
    添加 `LocalIpAddress` 属性获取本地 IP 地址。
    添加 `IP` 属性及相关方法获取和设置客户端 IP 地址。
    添加 `GetLanIP` 和 `GetLanIP(NetworkInterfaceType type)` 方法获取局域网 IP 地址。

--- Pek.AspNetCore/Helpers/DHWeb.cs
+++ Pek.AspNetCore/Helpers/DHWeb.cs
@@ -1,6 +1,10 @@
-﻿using System.Security.Claims;
+﻿using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Claims;
 
 using Pek.Security.Principals;
+using NewLife;
 
 namespace Pek.Helpers;
 
@@ -9,6 +13,37 @@ namespace Pek.Helpers;
 /// </summary>
 public static partial class DHWeb
 {
+    #region 构造函数
+    /// <summary>
+    /// 静态构造函数
+    /// </summary>
+    static DHWeb()
+    {
+        try
+        {
+            Environment = Pek.Webs.HttpContext.Current.RequestServices.GetService<IWebHostEnvironment>();
+            //ServicePointManager.DefaultConnectionLimit = 200;
+        }
+        catch
+        {
+        }
+    }
+    #endregion
+
+    #region Environment(宿主环境)
+    /// <summary>
+    /// 宿主环境
+    /// </summary>
+    public static IWebHostEnvironment Environment { get; set; }
+    #endregion
+
+    #region Request(当前Http请求)
+    /// <summary>
+    /// 当前Http请求
+    /// </summary>
+    public static HttpRequest Request => Pek.Webs.HttpContext.Current?.Request;
+    #endregion
+
     #region HttpContext(当前Http上下文)
 
     /// <summary>
@@ -50,6 +85,147 @@ public static partial class DHWeb
                 return identity;
             return UnauthenticatedIdentity.Instance;
         }
+    }
+
+    #endregion
+
+    #region 引用地址
+    /// <summary>
+    /// 引用地址
+    /// </summary>
+    public static string RefererUrl => Request.Headers["Referer"].FirstOrDefault();
+    #endregion
+
+    #region LocalIpAddress(本地IP)
+
+    /// <summary>
+    /// 本地IP
+    /// </summary>
+    public static string LocalIpAddress
+    {
+        get
+        {
+            try
+            {
+                var ipAddress = Webs.HttpContext.Current.Connection.LocalIpAddress;
+                return IPAddress.IsLoopback(ipAddress)
+                    ? IPAddress.Loopback.ToString()
+                    : ipAddress.MapToIPv4().ToString();
+            }
+            catch
+            {
+                return IPAddress.Loopback.ToString();
+            }
+        }
+    }
+
+    #endregion
+
+    #region IP(客户端IP地址)
+
+    /// <summary>
+    /// IP地址
+    /// </summary>
+    private static string _ip;
+
+    /// <summary>
+    /// 设置IP地址
+    /// </summary>
+    /// <param name="ip">IP地址</param>
+    public static void SetIp(string ip)
+    {
+        _ip = ip;
+    }
+
+    /// <summary>
+    /// 重置IP地址
+    /// </summary>
+    public static void ResetIp()
+    {
+        _ip = null;
+    }
+
+    /// <summary>
+    /// 客户端IP地址
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public static string IP
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(_ip) == false)
+            {
+                return _ip;
+            }
+            var list = new[] { "127.0.0.1", "::1" };
+            var result = Pek.Webs.HttpContext.Current?.Connection?.RemoteIpAddress.SafeString();
+            if (string.IsNullOrWhiteSpace(result) || list.Contains(result))
+            {
+                result = Runtime.Windows ? GetLanIP() : GetLanIP(NetworkInterfaceType.Ethernet);
+            }
+            if (result.Contains("::ffff:127.0.0.1"))
+            {
+                return "127.0.0.1";
+            }
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// 获取局域网IP
+    /// </summary>
+    /// <returns></returns>
+    // ReSharper disable once InconsistentNaming
+    private static string GetLanIP()
+    {
+        foreach (var hostAddress in Dns.GetHostAddresses(Dns.GetHostName()))
+        {
+            if (hostAddress.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return hostAddress.ToString();
+            }
+        }
+        return string.Empty;
+    }
+
+    /// <summary>
+    /// 获取局域网IP。
+    /// 参考地址：https://stackoverflow.com/questions/6803073/get-local-ip-address/28621250#28621250
+    /// 解决OSX下获取IP地址产生"Device not configured"的问题
+    /// </summary>
+    /// <param name="type">网络接口类型</param>
+    /// <returns></returns>
+    // ReSharper disable once InconsistentNaming
+    private static string GetLanIP(NetworkInterfaceType type)
+    {
+        try
+        {
+            foreach (var item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType != type || item.OperationalStatus != OperationalStatus.Up)
+                {
+                    continue;
+                }
+                var ipProperties = item.GetIPProperties();
+                if (ipProperties.GatewayAddresses.FirstOrDefault() == null)
+                {
+                    continue;
+                }
+                foreach (var ip in ipProperties.UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        return ip.Address.ToString();
+                    }
+                }
+            }
+        }
+        catch
+        {
+            return string.Empty;
+        }
+
+        return string.Empty;
     }
 
     #endregion
