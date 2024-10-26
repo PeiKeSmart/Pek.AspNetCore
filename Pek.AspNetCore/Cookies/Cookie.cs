c22521746d8dd3420c23efbb7a3e7c9f4a5e7897
猿人易 <qcjxberin@outlook.com>
2024-10-26 23:24:18 +0800

    添加 Cookie 和 SmartCookies 类以管理 HTTP Cookie
    在 `Cookie.cs` 文件中添加了 `Cookie` 类，用于处理 HTTP Cookie 的获取、设置和删除操作。引入了 `System.ComponentModel`、`NewLife` 和 `NewLife.Serialization` 命名空间，并实现了 `ICookie` 接口。
    
    在 `SmartCookies.cs` 文件中添加了 `SmartCookies` 类，实现了 `IDictionary<string, string>` 接口，用于更方便地管理 HTTP Cookie。引入了 `System.Collections` 和 `Microsoft.AspNetCore.Http` 命名空间，并提供了一个扩展方法 `AddSmartCookies` 用于将 `SmartCookies` 添加到依赖注入容器中。

---
+++ Pek.AspNetCore/Cookies/Cookie.cs
@@ -0,0 +1,138 @@
+﻿using System.ComponentModel;
+
+using NewLife;
+using NewLife.Serialization;
+
+namespace Pek.Cookies;
+
+public class Cookie : ICookie
+{
+    private readonly HttpContext _httpContext;
+    private const float DefaultExpireDurationMinutes = 43200; // 1 month
+    private const bool DefaultHttpOnly = true;
+    private const bool ExpireWithBrowser = false;
+    private const String DefaultPath = "/";
+
+    public Cookie(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContext = httpContextAccessor.HttpContext;
+    }
+
+    public T GetValue<T>(string name)
+    {
+        return GetValue<T>(name, false);
+    }
+
+    public T GetValue<T>(string name, bool expireOnceRead)
+    {
+        T value = default;
+
+        if (_httpContext.Request.Cookies.TryGetValue(name, out var valuStr))
+        {
+            if (!valuStr.IsNullOrWhiteSpace())
+            {
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+
+                try
+                {
+                    value = (T)converter.ConvertFromString(valuStr);
+                }
+                catch (NotSupportedException)
+                {
+                    try
+                    {
+                        value = JsonHelper.ToJsonEntity<T>(valuStr);
+                    }
+                    catch (NotSupportedException)
+                    {
+                        if (converter.CanConvertFrom(typeof(string)))
+                        {
+                            value = (T)converter.ConvertFrom(valuStr);
+                        }
+                    }
+                }
+            }
+
+            if (expireOnceRead)
+            {
+                Delete(name);
+            }
+        }
+
+        return value;
+    }
+
+    public void SetValue<T>(string name, T value)
+    {
+        SetValue(name, value, DefaultExpireDurationMinutes, DefaultHttpOnly, ExpireWithBrowser, DefaultPath);
+    }
+
+    public void SetValue<T>(string name, T value, String path)
+    {
+        SetValue(name, value, DefaultExpireDurationMinutes, DefaultHttpOnly, ExpireWithBrowser, path);
+    }
+
+    public void SetValue<T>(string name, T value, float expireDurationInMinutes)
+    {
+        SetValue(name, value, expireDurationInMinutes, DefaultHttpOnly, ExpireWithBrowser, DefaultPath);
+    }
+
+    public void SetValue<T>(string name, T value, float expireDurationInMinutes, String path)
+    {
+        SetValue(name, value, expireDurationInMinutes, DefaultHttpOnly, ExpireWithBrowser, path);
+    }
+
+    public void SetValue<T>(string name, T value, bool httpOnly, bool expireWithBrowser)
+    {
+        SetValue(name, value, DefaultExpireDurationMinutes, httpOnly, expireWithBrowser, DefaultPath);
+    }
+
+    public void SetValue<T>(string name, T value, bool httpOnly, bool expireWithBrowser, String path)
+    {
+        SetValue(name, value, DefaultExpireDurationMinutes, httpOnly, expireWithBrowser, path);
+    }
+
+    public void SetValue<T>(string name, T value, float expireDurationInMinutes, bool httpOnly, bool expireWithBrowser, String path)
+    {
+        var converter = TypeDescriptor.GetConverter(typeof(T));
+
+        var cookieValue = string.Empty;
+
+        try
+        {
+            cookieValue = converter.ConvertToString(value);
+        }
+        catch (NotSupportedException)
+        {
+            if (converter.CanConvertTo(typeof(string)))
+            {
+                cookieValue = (string)converter.ConvertTo(value, typeof(string));
+            }
+        }
+
+        if (!cookieValue.IsNullOrWhiteSpace())
+        {
+
+            if (expireWithBrowser)
+            {
+
+                _httpContext.Response.Cookies.Append(name, cookieValue);
+            }
+            else
+            {
+                _httpContext.Response.Cookies.Append(name, cookieValue, new CookieOptions
+                {
+                    Expires = DateTime.Now.AddMinutes(expireDurationInMinutes),
+                    HttpOnly = httpOnly,
+                    Path = path
+                });
+            }
+
+        }
+    }
+
+    public void Delete(string name)
+    {
+        _httpContext.Response.Cookies.Append(name, "", new CookieOptions { Expires = DateTime.Now.AddDays(-1d) });
+    }
+}
