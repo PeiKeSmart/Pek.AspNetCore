79317584ce44070f83e0cbe43dbb6170d909dc30
猿人易 <qcjxberin@outlook.com>
2024-11-09 17:20:16 +0800

    重构ServiceCollectionExtensions类以合并功能
    将AddAllSingletons方法返回类型改为void，并更新注释。添加对NewLife.Model命名空间的引用，扩展方法功能以遍历ObjectContainer.Current.Services集合。删除SingletonServiceExtensions类并合并其功能。

--- Pek.AspNetCore/Infrastructure/ServiceCollectionExtensions.cs
+++ Pek.AspNetCore/Infrastructure/ServiceCollectionExtensions.cs
@@ -1,18 +1,49 @@
-﻿namespace Pek.Infrastructure;
+﻿using NewLife.Model;
+
+namespace Pek.Infrastructure;
 
 public static class ServiceCollectionExtensions
 {
     /// <summary>
-    /// 添加静态编译的Singleton所有单例服务
+    /// 添加自定义DI容器所有单例服务
     /// </summary>
     /// <param name="services"></param>
     /// <returns></returns>
-    public static IServiceCollection AddAllSingletons(this IServiceCollection services)
+    public static void AddAllSingletons(this IServiceCollection services)
     {
-        foreach (var singleton in BaseSingleton.AllSingletons)
+        foreach (var kvp in BaseSingleton.AllSingletons)
         {
-            services.AddSingleton(singleton.Key, singleton.Value);
+            var serviceType = kvp.Key;
+            var implementationInstance = kvp.Value;
+
+            if (implementationInstance != null)
+            {
+                services.AddSingleton(serviceType, implementationInstance);
+            }
         }
-        return services;
+
+        foreach (var item in ObjectContainer.Current.Services)
+        {
+            var serviceType = item.ServiceType;
+            var implementationInstance = item.ImplementationType;
+
+            if (serviceType.GetInterfaces().Contains(typeof(IServiceProvider))) continue;
+
+            if (implementationInstance != null)
+            {
+                switch (item.Lifetime)
+                {
+                    case ObjectLifetime.Singleton:
+                        services.AddSingleton(serviceType, implementationInstance);
+                        break;
+                    case ObjectLifetime.Transient:
+                        services.AddTransient(serviceType, implementationInstance);
+                        break;
+                    case ObjectLifetime.Scoped:
+                        services.AddScoped(serviceType, implementationInstance);
+                        break;
+                }
+            }
+        }
     }
 }
