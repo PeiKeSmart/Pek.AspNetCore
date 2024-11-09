a24e6c150e4df9297635fe1fe9f6832b07f52a93
猿人易 <qcjxberin@outlook.com>
2024-11-09 16:49:40 +0800

    更新Pek.Common包版本并新增AddAllSingletons方法
    更新了Pek.AspNetCore.csproj文件中的Pek.Common包版本，从3.91.2024.1109-beta0336更新到3.91.2024.1109-beta0846。
    在SingletonServiceExtensions.cs文件中新增了一个扩展方法AddAllSingletons，用于将所有单例服务添加到IServiceCollection中。

---
+++ Pek.AspNetCore/Infrastructure/SingletonServiceExtensions.cs
@@ -0,0 +1,44 @@
+﻿using NewLife.Model;
+
+namespace Pek.Infrastructure;
+
+public static class SingletonServiceExtensions
+{
+    public static void AddAllSingletons(this IServiceCollection services)
+    {
+        foreach (var kvp in BaseSingleton.AllSingletons)
+        {
+            var serviceType = kvp.Key;
+            var implementationInstance = kvp.Value;
+
+            if (implementationInstance != null)
+            {
+                services.AddSingleton(serviceType, implementationInstance);
+            }
+        }
+
+        foreach (var item in ObjectContainer.Current.Services)
+        {
+            var serviceType = item.ServiceType;
+            var implementationInstance = item.ImplementationType;
+
+            if (serviceType.GetInterfaces().Contains(typeof(IServiceProvider))) continue;
+
+            if (implementationInstance != null)
+            {
+                switch (item.Lifetime)
+                {
+                    case ObjectLifetime.Singleton:
+                        services.AddSingleton(serviceType, implementationInstance);
+                        break;
+                    case ObjectLifetime.Transient:
+                        services.AddTransient(serviceType, implementationInstance);
+                        break;
+                    case ObjectLifetime.Scoped:
+                        services.AddScoped(serviceType, implementationInstance);
+                        break;
+                }
+            }
+        }
+    }
+}
