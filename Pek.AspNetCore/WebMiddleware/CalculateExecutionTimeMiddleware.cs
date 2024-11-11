19f84d20a7e0bbfb0e5ec5cd60242adf3a647626
猿人易 <qcjxberin@outlook.com>
2024-11-12 00:17:20 +0800

    启用可空引用类型并添加请求计时中间件
    在 `Pek.AspNetCore.csproj` 文件中启用了可空引用类型支持。
    新增 `CalculateExecutionTimeMiddleware` 中间件，用于记录 HTTP 请求的执行时间。
    引入了 `System.Diagnostics` 和 `NewLife.Log` 命名空间。
    定义了 `CalculateExecutionTimeMiddleware` 类，包含计时逻辑。
    添加了 `CalculateExecutionTimeMiddlewareExtensions` 扩展方法。

---
+++ Pek.AspNetCore/WebMiddleware/CalculateExecutionTimeMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+using NewLife.Log;
+
+namespace Pek.WebMiddleware;
+
+/// <summary>
+/// 记录执行时间的中间件
+/// </summary>
+public class CalculateExecutionTimeMiddleware
+{
+    private readonly RequestDelegate _next;  //下一个中间件
+    Stopwatch? stopwatch;
+
+    /// <summary>
+    /// 实例化
+    /// </summary>
+    /// <param name="next"></param>
+    public CalculateExecutionTimeMiddleware(RequestDelegate next) => _next = next ?? throw new ArgumentNullException(nameof(next));
+
+    /// <summary>
+    /// 调用
+    /// </summary>
+    /// <param name="ctx"></param>
+    /// <returns></returns>
+    public async Task Invoke(HttpContext ctx)
+    {
+        if (!ctx.WebSockets.IsWebSocketRequest)
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start(); //在下一个中间价处理前，启动计时器
+
+            await _next.Invoke(ctx);
+
+            stopwatch.Stop(); //所有的中间件处理完后，停止秒表。
+            XTrace.WriteLine($"请求{ctx.Request.Path}耗时{stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
+
+/// <summary>
+/// 记录执行时间的中间件扩展
+/// </summary>
+public static class CalculateExecutionTimeMiddlewareExtensions
+{
+    /// <summary>
+    /// 扩展
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IApplicationBuilder UseCalculateExecutionTime(this IApplicationBuilder app)
+    {
+        if (app == null)
+        {
+            throw new ArgumentNullException(nameof(app));
+        }
+        return app.UseMiddleware<CalculateExecutionTimeMiddleware>();
+    }
+}
