6bddc92a152de6bf7dfad0339a244d8524fef678
猿人易 <qcjxberin@outlook.com>
2024-11-20 20:50:43 +0800

    更新 Pek.Common 包版本并添加 MIME 映射功能
    更新了 Pek.Common 包的版本号至 4.0.2024.1120-beta1244。
    
    添加了 IMimeMapper 接口，定义了扩展 MIME 映射、根据扩展名获取 MIME 类型、根据路径获取 MIME 类型的方法。
    添加了 MimeMapper 类，实现了 IMimeMapper 接口，提供了默认 MIME 类型、扩展 MIME 映射规则、获取 MIME 类型和扩展名的方法。
    添加了 MimeMappingItem 类，表示一个 MIME 映射项，用于将文件扩展名映射到 MIME 类型。
    添加了 MimeTypes 静态类，定义了一些常用的 MIME 类型常量，避免拼写错误。

---
+++ Pek.AspNetCore/Mime/IMimeMapper.cs
@@ -0,0 +1,28 @@
+﻿namespace Pek.Mime;
+
+/// <summary>
+/// Mime
+/// </summary>
+public interface IMimeMapper
+{
+    /// <summary>
+    /// 扩展Mime
+    /// </summary>
+    /// <param name="extensions"></param>
+    /// <returns></returns>
+    IMimeMapper Extend(params MimeMappingItem[] extensions);
+
+    /// <summary>
+    /// 根据扩展名获取mime type
+    /// </summary>
+    /// <param name="fileExtension"></param>
+    /// <returns></returns>
+    String? GetMimeFromExtension(String fileExtension);
+
+    /// <summary>
+    /// 根据路径获取Mime Type
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
+    String? GetMimeFromPath(String filePath);
+}
