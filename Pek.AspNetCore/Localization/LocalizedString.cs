135e8868764d3ef3bc78504cf0918730fd3a2db7
猿人易 <qcjxberin@outlook.com>
2024-11-26 20:12:32 +0800

    重构 LocalizedString 类及其属性
    将 LocalizedString 类从继承 HtmlString 改为构造函数继承 HtmlString，并添加了 XML 注释。删除了原来的 Text 属性和构造函数，并将 Text 属性改为可空类型 String?，并在声明时初始化。

--- Pek.AspNetCore/Localization/LocalizedString.cs
+++ Pek.AspNetCore/Localization/LocalizedString.cs
@@ -5,19 +5,14 @@ namespace Pek.Localization;
 /// <summary>
 /// 本地化字符串
 /// </summary>
-public class LocalizedString : HtmlString
+/// <remarks>
+/// 实例化
+/// </remarks>
+/// <param name="localized">本地化内容</param>
+public class LocalizedString(String? localized) : HtmlString(localized)
 {
     /// <summary>
     /// 文本内容
     /// </summary>
-    public string Text { get; }
-
-    /// <summary>
-    /// 实例化
-    /// </summary>
-    /// <param name="localized">本地化内容</param>
-    public LocalizedString(string localized) : base(localized)
-    {
-        Text = localized;
-    }
+    public String? Text { get; } = localized;
 }
