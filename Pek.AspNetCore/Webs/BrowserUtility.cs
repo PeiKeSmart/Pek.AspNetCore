253c0e6c0c2e3033749da4ced904b6158c5003f5
猿人易 <qcjxberin@outlook.com>
2024-11-29 14:38:08 +0800

    更新 Pek.Common 包版本并新增 BrowserUtility 类
    更新了 Pek.Common 包版本至 4.0.2024.1129-beta0613。
    在 BrowserUtility.cs 文件中新增了 Pek.Webs 命名空间。
    新增了静态类 BrowserUtility，包含以下方法：
    - GetUserAgent(HttpRequest? httpRequest)：获取 User-Agent 字符串。
    - SideInWeixinBrowser(this Microsoft.AspNetCore.Http.HttpContext httpContext)：判断是否在微信内置浏览器中。
    - SideInWeixinMiniProgram(this Microsoft.AspNetCore.Http.HttpContext httpContext)：判断是否在微信小程序内发起请求（Android 有效，iOS 无效）。
    - SideInDingTalkBrowser(this Microsoft.AspNetCore.Http.HttpContext httpContext)：判断是否在钉钉内发起请求。

---
+++ Pek.AspNetCore/Webs/BrowserUtility.cs
@@ -0,0 +1,64 @@
+﻿namespace Pek.Webs;
+
+/// <summary>
+/// 浏览器公共类
+/// </summary>
+public static class BrowserUtility
+{
+    /// <summary>
+    /// 获取 Headers 中的 User-Agent 字符串
+    /// </summary>
+    /// <param name="httpRequest"></param>
+    /// <returns></returns>
+    public static String? GetUserAgent(HttpRequest? httpRequest)
+    {
+        String? userAgent = null;
+        var userAgentHeader = httpRequest?.Headers.UserAgent;
+        if (userAgentHeader?.Count > 0)
+        {
+            userAgent = userAgentHeader?[0]; //.ToUpper();
+        }
+        return userAgent;
+    }
+
+    /// <summary>
+    /// 判断是否在微信内置浏览器中
+    /// </summary>
+    /// <param name="httpContext">HttpContextBase对象</param>
+    /// <returns>true：在微信内置浏览器内。false：不在微信内置浏览器内。</returns>
+    public static Boolean SideInWeixinBrowser(this Microsoft.AspNetCore.Http.HttpContext httpContext)
+    {
+        httpContext ??= Helpers.DHWeb.HttpContext;
+        var userAgent = GetUserAgent(httpContext.Request)?.ToUpper();
+        //判断是否在微信浏览器内部
+        var isInWeixinBrowser = userAgent != null &&
+                    (!userAgent.Contains(" WXWORK/", StringComparison.OrdinalIgnoreCase) && (userAgent.Contains("MICROMESSENGER", StringComparison.OrdinalIgnoreCase) || userAgent.Contains("WINDOWS PHONE", StringComparison.OrdinalIgnoreCase)/*Windows Phone*/));
+        return isInWeixinBrowser;
+    }
+
+    /// <summary>
+    /// 判断是否在微信小程序内发起请求（注意：此方法在Android下有效，在iOS下暂时无效！）
+    /// </summary>
+    /// <param name="httpContext">HttpContextBase对象</param>
+    /// <returns>true：在微信内置浏览器内。false：不在微信内置浏览器内。</returns>
+    public static Boolean SideInWeixinMiniProgram(this Microsoft.AspNetCore.Http.HttpContext httpContext)
+    {
+        var userAgent = GetUserAgent(httpContext.Request)?.ToUpper();
+        //判断是否在微信小程序的 web-view 组件内部
+        var isInWeixinMiniProgram = userAgent != null && userAgent.Contains("MINIPROGRAM")/*miniProgram*/;
+        return isInWeixinMiniProgram;
+    }
+
+    /// <summary>
+    /// 判断是否在钉钉内发起请求
+    /// </summary>
+    /// <param name="httpContext">HttpContextBase对象</param>
+    /// <returns>true：在钉钉内置浏览器内。false：不在钉钉内置浏览器内。</returns>
+    public static Boolean SideInDingTalkBrowser(this Microsoft.AspNetCore.Http.HttpContext httpContext)
+    {
+        var userAgent = GetUserAgent(httpContext.Request)?.ToUpper();
+        //判断是否在微信小程序的 web-view 组件内部
+        var isInDingTalkBrowser = userAgent != null && userAgent.Contains("DingTalk")/*dingTalk*/;
+        return isInDingTalkBrowser;
+    }
+}
