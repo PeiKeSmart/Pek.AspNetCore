19d967e6685326f067b53c9711b7ec03b896de16
猿人易 <qcjxberin@outlook.com>
2024-12-06 07:58:27 +0800

    更新 DHWeb 类并升级 Pek.Common 包版本
    在 DHWeb.cs 文件中添加了多个静态属性和方法，改进了 HTTP 请求和响应的处理。更新了 Pek.AspNetCore.csproj 文件中的 Pek.Common 包版本，从 4.0.2024.1204-beta0354 升级到 4.0.2024.1205-beta1248。

--- Pek.AspNetCore/Helpers/DHWeb.cs
+++ Pek.AspNetCore/Helpers/DHWeb.cs
@@ -2,9 +2,15 @@
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Security.Claims;
+using System.Text;
+using System.Web;
+
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Net.Http.Headers;
 
 using NewLife;
 
+using Pek.IO;
 using Pek.Security.Principals;
 
 namespace Pek.Helpers;
@@ -45,12 +51,39 @@ public static partial class DHWeb
     public static HttpRequest? Request => Webs.HttpContext.Current?.Request;
     #endregion
 
+    #region Response(当前Http响应)
+
+    /// <summary>
+    /// 当前Http响应
+    /// </summary>
+    public static HttpResponse? Response => Pek.Webs.HttpContext.Current?.Response;
+
+    #endregion
+
     #region HttpContext(当前Http上下文)
 
     /// <summary>
     /// 当前Http上下文
     /// </summary>
     public static HttpContext HttpContext => Webs.HttpContext.Current;
+
+    #endregion
+
+    #region RequestType(请求类型)
+
+    /// <summary>
+    /// 请求类型
+    /// </summary>
+    public static String? RequestType => Pek.Webs.HttpContext.Current?.Request?.Method;
+
+    #endregion
+
+    #region Form(表单)
+
+    /// <summary>
+    /// Form表单
+    /// </summary>
+    public static IFormCollection? Form => Pek.Webs.HttpContext.Current?.Request?.Form;
 
     #endregion
 
@@ -90,11 +123,218 @@ public static partial class DHWeb
 
     #endregion
 
+    #region AccessToken(获取访问令牌)
+
+    /// <summary>
+    /// 获取访问令牌
+    /// </summary>
+    public static String? AccessToken
+    {
+        get
+        {
+            var authorization = Request?.Headers.Authorization;
+            if (String.IsNullOrWhiteSpace(authorization))
+                return null;
+            var list = authorization.SafeString().Split(' ');
+            if (list.Length == 2)
+                return list[1];
+            return null;
+        }
+    }
+
+    #endregion
+
+    #region Body(请求正文)
+
+    /// <summary>
+    /// 请求正文
+    /// </summary>
+    public static String Body
+    {
+        get
+        {
+            Request?.EnableBuffering();
+            return FileUtil.ToString(Request?.Body, isCloseStream: false);
+        }
+    }
+
+    /// <summary>
+    /// 获取请求正文
+    /// </summary>
+    /// <returns></returns>
+    public static async Task<String> GetBodyAsync()
+    {
+        Request?.EnableBuffering();
+        return await FileUtil.ToStringAsync(Request?.Body, isCloseStream: false).ConfigureAwait(false);
+    }
+
+    #endregion
+
+    #region UrlEncode(Url编码)
+
+    /// <summary>
+    /// Url编码
+    /// </summary>
+    /// <param name="url">url</param>
+    /// <param name="isUpper">编码字符是否转成大写，范例："http://"转成"http%3A%2F%2F"</param>
+    /// <returns></returns>
+    public static String UrlEncode(this String url, Boolean isUpper = false) => UrlEncode(url, Encoding.UTF8, isUpper);
+
+    /// <summary>
+    /// Url编码
+    /// </summary>
+    /// <param name="url">url</param>
+    /// <param name="encoding">字符编码</param>
+    /// <param name="isUpper">编码字符是否转成大写，范例："http://"转成"http%3A%2F%2F"</param>
+    /// <returns></returns>
+    public static String UrlEncode(this String url, String encoding, Boolean isUpper = false)
+    {
+        encoding = String.IsNullOrWhiteSpace(encoding) ? "UTF-8" : encoding;
+        return UrlEncode(url, Encoding.GetEncoding(encoding), isUpper);
+    }
+
+    /// <summary>
+    /// Url编码
+    /// </summary>
+    /// <param name="url">url</param>
+    /// <param name="encoding">字符编码</param>
+    /// <param name="isUpper">编码字符是否转成大写，范例："http://"转成"http%3A%2F%2F"</param>
+    /// <returns></returns>
+    public static String UrlEncode(this String url, Encoding encoding, Boolean isUpper = false)
+    {
+        var result = HttpUtility.UrlEncode(url, encoding);
+        if (isUpper == false)
+        {
+            return result;
+        }
+
+        return GetUpperEncode(result);
+    }
+
+    /// <summary>
+    /// 获取大写编码字符串
+    /// </summary>
+    /// <param name="encode">编码字符串</param>
+    /// <returns></returns>
+    private static String GetUpperEncode(String encode)
+    {
+        var result = new StringBuilder();
+        var index = Int32.MinValue;
+        for (var i = 0; i < encode.Length; i++)
+        {
+            var character = encode[i].ToString();
+            if (character == "%")
+            {
+                index = i;
+            }
+
+            if (i - index == 1 || i - index == 2)
+            {
+                character = character.ToUpper();
+            }
+
+            result.Append(character);
+        }
+
+        return result.ToString();
+    }
+
+    #endregion
+
+    #region UrlDecode(Url解码)
+
+    /// <summary>
+    /// Url解码
+    /// </summary>
+    /// <param name="url">url</param>
+    /// <returns></returns>
+    public static String UrlDecode(this String url) => HttpUtility.UrlDecode(url);
+
+    /// <summary>
+    /// Url解码
+    /// </summary>
+    /// <param name="url">url</param>
+    /// <param name="encoding">字符编码</param>
+    /// <returns></returns>
+    public static String UrlDecode(this String url, Encoding encoding) => HttpUtility.UrlDecode(url, encoding);
+
+    #endregion
+
     #region 引用地址
     /// <summary>
     /// 引用地址
     /// </summary>
-    public static String? RefererUrl => Request?.Headers["Referer"].FirstOrDefault();
+    public static String? RefererUrl => Request?.Headers.Referer;
+    #endregion
+
+    #region Url(请求地址)
+
+    /// <summary>
+    /// 获得请求的原始url(未转义)
+    /// </summary>
+    public static String? Url => Request?.GetDisplayUrl();
+
+    /// <summary>
+    /// 获得请求的原始url(转义)
+    /// </summary>
+    public static String? EncodedUrl => Request?.GetEncodedUrl();
+
+    #endregion
+
+    #region Host(主机)
+
+    /// <summary>
+    /// 主机
+    /// </summary>
+    public static String Host => Pek.Webs.HttpContext.Current == null ? Dns.GetHostName() : GetClientHostName();
+
+    /// <summary>
+    /// 获取Web客户端主机名
+    /// </summary>
+    /// <returns></returns>
+    private static String GetClientHostName()
+    {
+        var address = GetRemoteAddress();
+        if (String.IsNullOrWhiteSpace(address))
+        {
+            return Dns.GetHostName();
+        }
+        var result = Dns.GetHostEntry(IPAddress.Parse(address)).HostName;
+        if (result == "localhost.localdomain")
+        {
+            result = Dns.GetHostName();
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// 获取远程地址
+    /// </summary>
+    /// <returns></returns>
+    private static String? GetRemoteAddress()
+    {
+        return Pek.Webs.HttpContext.Current?.Request?.Headers["HTTP_X_FORWARDED_FOR"] ??
+               Pek.Webs.HttpContext.Current?.Request?.Headers["REMOTE_ADDR"];
+    }
+
+    #endregion
+
+    #region ContentType(内容类型)
+
+    /// <summary>
+    /// 内容类型
+    /// </summary>
+    public static String? ContentType => Pek.Webs.HttpContext.Current?.Request?.ContentType;
+
+    #endregion
+
+    #region QueryString(参数)
+
+    /// <summary>
+    /// 参数
+    /// </summary>
+    public static String? QueryString => Pek.Webs.HttpContext.Current?.Request?.QueryString.ToString();
+
     #endregion
 
     #region LocalIpAddress(本地IP)
@@ -223,6 +463,45 @@ public static partial class DHWeb
 
     #endregion
 
+    #region IsLocal(是否本地请求)
+
+    /// <summary>
+    /// 是否本地请求
+    /// </summary>
+    public static Boolean IsLocal
+    {
+        get
+        {
+            var connection = Pek.Webs.HttpContext.Current?.Request?.HttpContext?.Connection;
+            if (connection == null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+
+            if (connection.RemoteIpAddress?.IsSet() == true)
+            {
+                return connection.LocalIpAddress?.IsSet() == true
+                    ? connection.RemoteIpAddress.Equals(connection.LocalIpAddress)
+                    : IPAddress.IsLoopback(connection.RemoteIpAddress);
+            }
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// 空IP地址
+    /// </summary>
+    private const String NullIpAddress = "::1";
+
+    /// <summary>
+    /// 是否已设置IP地址
+    /// </summary>
+    /// <param name="address">IP地址</param>
+    private static Boolean IsSet(this IPAddress address) => address != null && address.ToString() != NullIpAddress;
+
+    #endregion
+
     /// <summary>
     /// 获取当前站点Url
     /// </summary>
@@ -253,14 +532,278 @@ public static partial class DHWeb
     /// Web客户端，用于发送Http请求
     /// </summary>
     /// <returns></returns>
-    public static Pek.Webs.Clients.WebClient Client() => new Pek.Webs.Clients.WebClient();
+    public static Pek.Webs.Clients.WebClient Client() => new();
 
     /// <summary>
     /// Web客户端，用于发送Http请求
     /// </summary>
     /// <typeparam name="TResult">返回结果类型</typeparam>
     /// <returns></returns>
-    public static Pek.Webs.Clients.WebClient<TResult> Client<TResult>() where TResult : class => new Pek.Webs.Clients.WebClient<TResult>();
+    public static Pek.Webs.Clients.WebClient<TResult> Client<TResult>() where TResult : class => new();
+
+    #endregion
+
+    #region DownloadAsync(下载)
+
+    /// <summary>
+    /// 下载文件
+    /// </summary>
+    /// <param name="filePath">文件绝对路径</param>
+    /// <param name="fileName">文件名。包含扩展名</param>
+    public static async Task DownloadFileAsync(String filePath, String fileName) => await DownloadFileAsync(filePath, fileName, Encoding.UTF8).ConfigureAwait(false);
+
+    /// <summary>
+    /// 下载文件
+    /// </summary>
+    /// <param name="filePath">文件绝对路径</param>
+    /// <param name="fileName">文件名。包含扩展名</param>
+    /// <param name="encoding">字符编码</param>
+    public static async Task DownloadFileAsync(String filePath, String fileName, Encoding encoding)
+    {
+        var bytes = FileUtil.ReadToBytes(filePath);
+        await DownloadAsync(bytes, fileName, encoding).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// 下载
+    /// </summary>
+    /// <param name="stream">流</param>
+    /// <param name="fileName">文件名。包含扩展名</param>
+    public static async Task DownloadAsync(Stream stream, String fileName) => await DownloadAsync(stream, fileName, Encoding.UTF8).ConfigureAwait(false);
+
+    /// <summary>
+    /// 下载
+    /// </summary>
+    /// <param name="stream">流</param>
+    /// <param name="fileName">文件名。包含扩展名</param>
+    /// <param name="encoding">字符编码</param>
+    public static async Task DownloadAsync(Stream stream, String fileName, Encoding encoding) => await DownloadAsync(await FileUtil.ToBytesAsync(stream).ConfigureAwait(false), fileName, encoding).ConfigureAwait(false);
+
+    /// <summary>
+    /// 下载
+    /// </summary>
+    /// <param name="bytes">字节流</param>
+    /// <param name="fileName">文件名。包含扩展名</param>
+    public static async Task DownloadAsync(Byte[] bytes, String fileName) => await DownloadAsync(bytes, fileName, Encoding.UTF8).ConfigureAwait(false);
+
+    /// <summary>
+    /// 下载
+    /// </summary>
+    /// <param name="bytes">字节流</param>
+    /// <param name="fileName">文件名。包含扩展名</param>
+    /// <param name="encoding">字符编码</param>
+    /// <returns></returns>
+    public static async Task DownloadAsync(Byte[]? bytes, String fileName, Encoding encoding)
+    {
+        if (bytes == null || bytes.Length == 0)
+        {
+            return;
+        }
+
+        if (Response == null)
+        {
+            return;
+        }
+
+        fileName = fileName.Replace(" ", "");
+        fileName = UrlEncode(fileName, encoding);
+        Response.ContentType = "application/octet-stream";
+
+        Response.Headers[HeaderNames.ContentDisposition] = new ContentDispositionHeaderValue("attachment")
+        {
+            FileNameStar = fileName
+        }.ToString();
+        Response.Headers.ContentLength = bytes.Length;
+
+        await Response.Body.WriteAsync(bytes).ConfigureAwait(false);
+    }
+
+    #endregion
+
+    #region GetFiles(获取客户端文件集合)
+
+    /// <summary>
+    /// 获取客户端文件集合
+    /// </summary>
+    /// <returns></returns>
+    public static List<IFormFile> GetFiles()
+    {
+        var result = new List<IFormFile>();
+        var files = Pek.Webs.HttpContext.Current.Request.Form.Files;
+        if (files == null || files.Count == 0)
+        {
+            return result;
+        }
+
+        result.AddRange(files.Where(file => file?.Length > 0));
+        return result;
+    }
+
+    #endregion
+
+    #region GetFile(获取客户端文件)
+
+    /// <summary>
+    /// 获取客户端文件
+    /// </summary>
+    /// <returns></returns>
+    public static IFormFile? GetFile()
+    {
+        var files = GetFiles();
+        return files.Count == 0 ? null : files[0];
+    }
+
+    #endregion
+
+    #region GetParam(获取请求参数)
+
+    /// <summary>
+    /// 获取请求参数，搜索路径：查询参数->表单参数->请求头
+    /// </summary>
+    /// <param name="name">参数名</param>
+    public static String? GetParam(String name)
+    {
+        if (String.IsNullOrWhiteSpace(name))
+            return String.Empty;
+        if (Request == null)
+            return String.Empty;
+        var result = String.Empty;
+        if (Request.Query != null)
+            result = Request.Query[name];
+        if (String.IsNullOrWhiteSpace(result) == false)
+            return result;
+        if (Request.Form != null)
+            result = Request.Form[name];
+        if (String.IsNullOrWhiteSpace(result) == false)
+            return result;
+        if (Request.Headers != null)
+            result = Request.Headers[name];
+        return result;
+    }
+
+    #endregion
+
+    #region Redirect(跳转到指定链接)
+
+    /// <summary>
+    /// 跳转到指定链接
+    /// </summary>
+    /// <param name="url">链接</param>
+    public static void Redirect(String url) => Response?.Redirect(url);
+
+    #endregion
+
+    #region Write(输出内容)
+
+    /// <summary>
+    /// 输出内容
+    /// </summary>
+    /// <param name="text">内容</param>
+    public static void Write(String text)
+    {
+        if (Response == null)
+            return;
+
+        Response.ContentType = "text/plain;charset=utf-8";
+        Task.Run(async () => { await Response.WriteAsync(text).ConfigureAwait(false); }).GetAwaiter().GetResult();
+    }
+
+    #endregion
+
+    #region Write(输出文件)
+
+    /// <summary>
+    /// 输出文件
+    /// </summary>
+    /// <param name="stream">文件流</param>
+    public static void Write(FileStream stream)
+    {
+        if (Response == null) return;
+
+        var size = stream.Length;
+        var buffer = new Byte[size];
+        _ = stream.Read(buffer, 0, (Int32)size);
+        stream.Dispose();
+        File.Delete(stream.Name);
+
+        Response.ContentType = "application/octet-stream";
+        Response.Headers[HeaderNames.ContentDisposition] = new ContentDispositionHeaderValue("attachment")
+        {
+            FileNameStar = WebUtility.UrlEncode(Path.GetFileName(stream.Name))
+        }.ToString();
+        Response.Headers.ContentLength = size;
+
+        Task.Run(async () => { await Response.Body.WriteAsync(buffer.AsMemory(0, (Int32)size)).ConfigureAwait(false); }).GetAwaiter().GetResult();
+        Response.Body.Close();
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 返回绝对地址
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    public static String AbsoluteUri(this HttpRequest request)
+    {
+        var absoluteUri = String.Concat(
+                      request.Scheme,
+                      "://",
+                      request.Host.ToUriComponent(),
+                      request.PathBase.ToUriComponent(),
+                      request.Path.ToUriComponent(),
+                      request.QueryString.ToUriComponent());
+
+        return absoluteUri;
+    }
+
+    public enum AgentType
+    {
+        Android = 0,
+        IPhone = 1,
+        IPad = 2,
+        WindowsPhone = 3,
+        Windows = 4,
+        Wechat = 6,
+        MacOS = 7
+    }
+
+    /// <summary>
+    /// 获取客户端信息
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    public static AgentType UserAgentType(this HttpRequest request)
+    {
+        var userAgent = request.Headers.UserAgent;
+        switch (userAgent)
+        {
+            case String android when android.Contains("MicroMessenger"):
+                return AgentType.Wechat;
+            case String android when android.Contains("Android"):
+                return AgentType.Android;
+            case String android when android.Contains("iPhone"):
+                return AgentType.IPhone;
+            case String android when android.Contains("iPad"):
+                return AgentType.IPad;
+            case String android when android.Contains("Windows Phone"):
+                return AgentType.WindowsPhone;
+            case String android when android.Contains("Windows NT"):
+                return AgentType.Windows;
+            case String android when android.Contains("Mac OS"):
+                return AgentType.MacOS;
+            default:
+                break;
+        }
+        return AgentType.Android;
+    }
+
+    #region UserAgent(用户代理)
+
+    /// <summary>
+    /// 用户代理
+    /// </summary>
+    public static String? UserAgent => Request?.Headers.UserAgent;
 
     #endregion
 }
