0ffba7f55a5fce7652b8cf568bf17eca6b3b5476
猿人易 <qcjxberin@outlook.com>
2024-12-07 14:18:38 +0800

    更新Pek.Common包版本并添加HelperExtensions类
    更新了Pek.Common包的版本，从4.0.2024.1206-beta1005到4.0.2024.1207-beta0606。
    在HelperExtensions.cs文件中添加了using Microsoft.AspNetCore.Mvc.ViewFeatures引用。
    添加了Pek.Extensions命名空间。
    添加了静态类HelperExtensions，并添加了以下扩展方法：
    - ToEnvVariable：将字符串列表转换为环境变量格式的字符串。
    - CopyTo：从StreamReader复制内容到StreamWriter，支持指定缓冲区大小。
    - AddDictionary：将字典中的键值对添加到AttributeDictionary中。
    - DictionaryEqual：比较两个字典是否相等，支持可选的值比较器。

---
+++ Pek.AspNetCore/Extensions/HelperExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Pek.Extensions;
+
+public static class HelperExtensions
+{
+    public static String ToEnvVariable(this IEnumerable<String> stringList, String seperator = ",") => String.Join(seperator, stringList.Select(d => d.Replace(@"\", @"\\").Replace(seperator, $@"\{seperator}")));
+
+    public static void CopyTo(this StreamReader reader, StreamWriter writer, Int32 bufferSize = 4096)
+    {
+        var buffer = new Char[bufferSize];
+        Int32 read;
+        while ((read = reader.Read(buffer, 0, bufferSize)) > 0)
+        {
+            writer.Write(buffer, 0, read);
+        }
+    }
+
+    public static void AddDictionary<TValue>(this AttributeDictionary attrDictionary, IDictionary<String, TValue> dictionary)
+    {
+        foreach (var kvp in dictionary)
+            attrDictionary.Add(kvp.Key, kvp.Value?.ToString());
+    }
+
+    public static Boolean DictionaryEqual<TKey, TValue>(
+        this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second) => first.DictionaryEqual(second, null);
+
+    public static Boolean DictionaryEqual<TKey, TValue>(
+        this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second,
+        IEqualityComparer<TValue>? valueComparer)
+    {
+        if (first == second) return true;
+        if ((first == null) || (second == null)) return false;
+        if (first.Count != second.Count) return false;
+
+        valueComparer ??= EqualityComparer<TValue>.Default;
+
+        foreach (var kvp in first)
+        {
+            if (!second.TryGetValue(kvp.Key, out var secondValue)) return false;
+            if (!valueComparer.Equals(kvp.Value, secondValue)) return false;
+        }
+        return true;
+    }
+}
