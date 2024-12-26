39405d5a2fcd0d5f317b5aaa72183c0012215b29
猿人易 <qcjxberin@outlook.com>
2024-12-26 15:45:12 +0800

    重命名接口并更新依赖包版本
    将 IDHStartup 接口重命名为 IPekStartup，并继承自 IDHStartup。移除了 IDHStartup 接口中的多个方法和属性。将 DHConast 类中的 DHStartups 属性类型改为 IEnumerable<IDHStartup>?，允许其为 null。更新 Pek.AspNetCore.csproj 文件中的 Pek.Common 包版本，从 4.0.2024.1226-beta0348 升级到 4.0.2024.1226-beta0741。

--- Pek.AspNetCore/Infrastructure/IDHStartup.cs
+++ Pek.AspNetCore/Infrastructure/IDHStartup.cs
@@ -1,11 +1,9 @@
-﻿using Pek.VirtualFileSystem;
-
-namespace Pek.Infrastructure;
+﻿namespace Pek.Infrastructure;
 
 /// <summary>
 /// 表示应用程序启动时配置服务和中间件的对象
 /// </summary>
-public interface IDHStartup
+public interface IPekStartup: IDHStartup
 {
     /// <summary>
     /// 添加并配置任何中间件
@@ -22,31 +20,10 @@ public interface IDHStartup
     void Configure(IApplicationBuilder application);
 
     /// <summary>
-    /// 配置虚拟文件系统
-    /// </summary>
-    /// <param name="options">虚拟文件配置</param>
-    void ConfigureVirtualFileSystem(DHVirtualFileSystemOptions options);
-
-    /// <summary>
     /// 注册路由
     /// </summary>
     /// <param name="endpoints">路由生成器</param>
     void UseDHEndpoints(IEndpointRouteBuilder endpoints);
-
-    /// <summary>
-    /// 将区域路由写入数据库
-    /// </summary>
-    void ConfigureArea();
-
-    /// <summary>
-    /// 调整菜单
-    /// </summary>
-    void ChangeMenu();
-
-    /// <summary>
-    /// 升级处理逻辑
-    /// </summary>
-    void Update();
 
     /// <summary>
     /// 配置使用添加的中间件
@@ -66,19 +43,11 @@ public interface IDHStartup
     /// <param name="application"></param>
     void AfterAuth(IApplicationBuilder application);
 
-    /// <summary>
-    /// 获取此启动配置实现的顺序
-    /// </summary>
-    int StartupOrder { get; }
-
-    /// <summary>
-    /// 获取此启动配置实现的顺序。主要针对ConfigureMiddleware、UseRouting前执行的数据、UseAuthentication或者UseAuthorization后面 Endpoints前执行的数据
-    /// </summary>
-    int ConfigureOrder { get; }
+    
 }
 
 public class DHConast
 {
     /// <summary>IDHStartup集合</summary>
-    public static IEnumerable<IDHStartup> DHStartups { get; set; }
+    public static IEnumerable<IDHStartup>? DHStartups { get; set; }
 }
