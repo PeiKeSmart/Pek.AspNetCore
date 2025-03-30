9e889b3acae370da1c6f962750ed935572827ee2
猿人易 <qcjxberin@outlook.com>
2025-03-30 14:09:27 +0800

    添加自定义压缩提供程序选择器
    实现了 `BrowserCompatibleCompressionProvider` 类，支持根据客户端的 `Accept-Encoding` 请求头选择 Brotli 或 Gzip 压缩。该类实现了 `ICompressionProvider` 接口，并包含必要的构造函数、属性和方法以支持流的创建和压缩格式的选择。

---
+++ Pek.AspNetCore/Compression/BrowserCompatibleCompressionProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+
+namespace Pek.Compression;
+
+/// <summary>
+/// 自定义压缩提供程序选择器
+/// </summary>
+public class BrowserCompatibleCompressionProvider : ICompressionProvider
+{
+    private readonly BrotliCompressionProvider _brotliProvider;
+    private readonly GzipCompressionProvider _gzipProvider;
+    private readonly HttpContext? _httpContext;
+
+    public BrowserCompatibleCompressionProvider(
+        BrotliCompressionProvider brotliProvider,
+        GzipCompressionProvider gzipProvider,
+        IHttpContextAccessor httpContextAccessor)
+    {
+        _brotliProvider = brotliProvider;
+        _gzipProvider = gzipProvider;
+        _httpContext = httpContextAccessor.HttpContext;
+    }
+
+    public String EncodingName =>
+        SupportsBrotli() ? _brotliProvider.EncodingName : _gzipProvider.EncodingName;
+
+    public Boolean SupportsFlush => true;
+
+    public Stream CreateStream(Stream outputStream)
+    {
+        return SupportsBrotli()
+            ? _brotliProvider.CreateStream(outputStream)
+            : _gzipProvider.CreateStream(outputStream);
+    }
+
+    private Boolean SupportsBrotli()
+    {
+        if (_httpContext == null)
+            return false;
+
+        var acceptEncoding = _httpContext.Request.Headers["Accept-Encoding"].ToString().ToLowerInvariant();
+        return acceptEncoding.Contains("br");
+    }
+}
