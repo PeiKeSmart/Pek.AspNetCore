2641e46a21046f74f902ad8cd41c3bdf5ed4b867
IOT8 <qcjxberin@outlook.com>
2025-09-23 21:58:58 +0800

    新增 FieldsHelper 类以支持字段相关操作
    新增 FieldsHelper 类，位于 Pek 命名空间中，包含两个静态属性：
    1. AreaNames：用于存储区域名集合。
    2. Assemblies：用于存储程序集集合，支持通过依赖注入动态获取程序集信息。
    此外，添加了必要的命名空间引用，并为类提供了 XML 文档注释。

---
+++ Pek.AspNetCore/Helpers/FieldsHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+using NewLife.Model;
+
+using Pek.Infrastructure;
+
+namespace Pek;
+
+/// <summary>
+/// 字段帮助类
+/// </summary>
+public class FieldsHelper
+{
+    /// <summary>区域名集合</summary>
+    public static String[]? AreaNames { get; set; }
+
+    /// <summary>文件Assembly集合</summary>
+    public static IList<Assembly> Assemblies { get; set; } = ObjectContainer.Provider.GetPekService<ITypeFinder>()?.GetAssemblies() ?? [];
+}
